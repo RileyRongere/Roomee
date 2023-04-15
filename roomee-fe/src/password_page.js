99de8ec2d2ca121d9bea4292a20103b40704b6c6
Amelia Sims <amelia.sims@drake.edu>
2023-04-15 17:04:54 -0500

    Created Password Page File

---
+++ roomee-fe/src/password_page.js
@@ -0,0 +1,8 @@
+function searchPassword() {
+    api_call_mock.getElementById("password")
+    // this sends input passowrd to the api, to check against saved password 
+    // or add a new password for new user 
+}
+
+// how are we going to handle the password being incorrect? 
+// we will need some feedback for the user if incorrect 
