31dfd5830b660896e3dce97a72fd8d1bf7e0ef98
Marcus Andrist <marcus.andrist@drake.edu>
2023-04-17 14:44:08 -0500

    Added a test for basic /hello and a test client fixture

--- server/roomee_api/test/test_roomie_api.py
+++ server/roomee_api/test/test_roomie_api.py
@@ -1,3 +1,23 @@
-from src.roomee_api import app
-def test_dummy():
-    pass
+from roomee_api import app
+from pytest import fixture
+
+# test client
+@fixture
+def test_client():
+    return app.test_client()
+
+
+# Integration tests for client-app interaction
+
+def test_hello_world(test_client):
+
+    # create a test client
+    #client = app.test_client()
+
+    # make a GET request to the `/hello` endpoint
+    response = test_client.get('/hello')
+
+    # check if response status code is 200 and contains bytes object
+    assert response.status_code == 200
+
+    assert b'Hello, World!' in response.data
