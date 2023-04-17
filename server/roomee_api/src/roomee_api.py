20db45620633f83ff916221f6fac770f6c9edd01
Marcus Andrist <marcus.andrist@drake.edu>
2023-04-17 15:26:16 -0500

    Added user id list GET and check if user exists POST + tests

--- server/roomee_api/src/roomee_api.py
+++ server/roomee_api/src/roomee_api.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
 
-
 @app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Method to return every user to the endpoint /api/users
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    # Mock list of user ids 
+    user_ids = []
+    
+    return jsonify({'user_ids': user_ids})
+
+# Method to return every user to the endpoint /api/users
+@app.route('/users', methods=['POST'])
+def check_user_exists():
+
+    # Fetch user_id to check from response
+    data = request.get_json()
+    user_id = data['user_id']
+
+    # Mock list of user ids 
+    user_ids = [37, 43, 1009]
+    
+    if user_id in user_ids:
+        return jsonify({'user_exists': True})
+    else:
+        return jsonify({'user_exists': False})
--- server/roomee_api/test/test_roomie_api.py
+++ server/roomee_api/test/test_roomie_api.py
@@ -1,5 +1,6 @@
 from roomee_api import app
 from pytest import fixture
+import json
 
 # test client
 @fixture
@@ -21,3 +22,36 @@ def test_hello_world(test_client):
     assert response.status_code == 200
 
     assert b'Hello, World!' in response.data
+
+
+def test_get_users(test_client):
+
+    # make a GET request to the `/users` endpoint
+    response = test_client.get('/hello')
+
+    # check if response status code is 200 and contains bytes object
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data
+
+# Tests if user exists using a both a valid and invalid id
+def test_check_user(test_client):
+
+    # Create params for POST
+    headers = {'Content-Type': 'application/json'}
+    payload = {'user_id': 37}
+    false_payload = {'user_id': 66}
+
+    # Create POSTs
+    response = test_client.post('/users', json=payload, headers=headers)
+    data = json.loads(response.data.decode())
+
+    false_response = test_client.post('/users', json=false_payload, headers=headers)
+    false_data = json.loads(response.data.decode())
+
+    # Good response and a True boolean
+    assert response.status_code == 200
+    assert data['user_exists']
+
+    # Good response and a False boolean
+    assert false_response.status_code == 200
+    assert false_data['user_exists']
