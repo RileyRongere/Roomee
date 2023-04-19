b288886632ab1c9dcebb0aa5170086af20e1d930
Marcus Andrist <marcus.andrist@drake.edu>
2023-04-19 16:41:43 -0500

    Ran Black.

--- server/roomee_api/src/roomee_api.py
+++ server/roomee_api/src/roomee_api.py
@@ -4,75 +4,78 @@ import os
 
 app = Flask(__name__)
 
-DATABASE = 'roomee.db'
+DATABASE = "roomee.db"
 
 # Mocked methods for use by FE team
 
-# POST a dict with (Check test file for exact structure of data)
-@app.route('/questions', methods=['POST'])
-def post_questions():
 
+# POST a dict with (Check test file for exact structure of data)
+@app.route("/questions", methods=["POST"])
+def post_questions():
     data = request.get_json()
     # Check if all required elements are present
     try:
-        user_id = data['user_id']
+        user_id = data["user_id"]
 
-        question_id = data['question_id']
-        value = data['value']
+        question_id = data["question_id"]
+        value = data["value"]
 
     except KeyError as e:
         missing_param = str(e).strip("'")
-        return jsonify({'error': f'Missing parameter: {missing_param}'}), 400
+        return jsonify({"error": f"Missing parameter: {missing_param}"}), 400
 
     # Return a success message or the created user object
-    return jsonify({'message': 'User created successfully'}), 201
+    return jsonify({"message": "User created successfully"}), 201
+
 
 # Fetch local database
 def get_db():
-
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(DATABASE)
     return g.db
+
 
 # Close DB when app closes
 @app.teardown_appcontext
 def close_db(e=None):
-
-    db = g.pop('db', None)
+    db = g.pop("db", None)
     if db is not None:
         db.close()
 
+
 # Return type as of now is a list of lists [[first_name, last_name, gender], [...]]
-@app.route('/users', methods=['GET'])
+@app.route("/users", methods=["GET"])
 def get_users():
     db = get_db()
     cur = db.cursor()
-    cur.execute('SELECT * FROM users')
+    cur.execute("SELECT * FROM users")
     users = cur.fetchall()
     cur.close()
     return jsonify(users)
 
+
 # POST a dict with (a param from line 4-6)
-@app.route('/users', methods=['POST'])
+@app.route("/users", methods=["POST"])
 def create_user():
     data = request.get_json()
-    first_name = data.get('first_name') # 4-6
-    last_name = data.get('last_name')
-    gender = data.get('gender')
+    first_name = data.get("first_name")  # 4-6
+    last_name = data.get("last_name")
+    gender = data.get("gender")
 
     # Insert the user data into the database
     db = get_db()
     cur = db.cursor()
-    query = '''
+    query = """
     INSERT INTO users (first_name, last_name, gender)
     VALUES (?, ?, ?)
-    '''
+    """
     cur.execute(query, (first_name, last_name, gender))
     db.commit()
     cur.close()
 
     # Return a success message or the created user object
-    return jsonify({'message': 'User created successfully'}), 201
+    return jsonify({"message": "User created successfully"}), 201
+
 
 @app.route("/hello")
 def hello_world():
--- server/roomee_api/test/test_roomie_api.py
+++ server/roomee_api/test/test_roomie_api.py
@@ -2,6 +2,7 @@ from roomee_api import app
 from pytest import fixture
 import json
 
+
 @fixture
 def test_client():
     return app.test_client()
@@ -16,10 +17,11 @@ def test_get_users(test_client):
     assert response.status_code == 200
     assert type(data) == list
 
+
 def test_post_users(test_client):
     # Create params for POST
     headers = {"Content-Type": "application/json"}
-    payload = {"first_name": "name", "last_name": "name2", "gender":"female"}
+    payload = {"first_name": "name", "last_name": "name2", "gender": "female"}
     false_payload = {"user_id": 66}
 
     # Create POSTs
@@ -30,18 +32,20 @@ def test_post_users(test_client):
     false_data = json.loads(response.data.decode())
 
     # Good response and a True boolean
-    assert response.status_code == 201 # Successful POST
+    assert response.status_code == 201  # Successful POST
     assert type(data) == dict
 
     # Bad response
-    assert false_response.status_code == 201 # Successful POST, No payload schema implemented
+    assert (
+        false_response.status_code == 201
+    )  # Successful POST, No payload schema implemented
     assert type(data) == dict
 
 
 def test_post_questions(test_client):
     # Create params for POST
     headers = {"Content-Type": "application/json"}
-    payload = {"question_id":1, "user_id":1234, "value": 13}
+    payload = {"question_id": 1, "user_id": 1234, "value": 13}
     false_payload = {"question_id": 66, "value": 13}
 
     # Create POSTs
@@ -52,9 +56,9 @@ def test_post_questions(test_client):
     false_data = json.loads(false_response.data.decode())
 
     # Good response and a True boolean
-    assert response.status_code == 201 # Successful POST
+    assert response.status_code == 201  # Successful POST
     assert type(data) == dict
 
     # Bad response
-    assert false_response.status_code == 400 # Error
+    assert false_response.status_code == 400  # Error
     assert false_data == {"error": "Missing parameter: user_id"}
