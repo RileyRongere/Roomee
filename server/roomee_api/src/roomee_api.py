5d3f9876d2f6ecf7ec4e3094c8af57dc0cfabe09
Kieran Smith <kieran.smith@drake.edu>
2023-04-26 13:19:47 -0500

    various cleanup of unneeded endpoints and functions

--- server/roomee_api/src/roomee_api.py
+++ server/roomee_api/src/roomee_api.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # method to return user from the insert_query_DB.py file
 # note that this does not check if the user has been created.
 def get_user(username):
+    is_valid(username)
     user = query_user(username)
     return user
 
@@ -31,36 +32,10 @@ def is_valid(dirty_string):
 
 
 
-# Method to return every user to the endpoint /api/users
-
-# This probably doesn't need an endpoint associated with it. I don't think it will be called from outside the API -Kieran
-@app.route("/users", methods=["GET"])
-def get_users():
-    # Mock list of user ids
-    user_ids = []
-
-    return jsonify({"user_ids": user_ids})
-
-
 # Method to return a boolean to the endpoint /api/users if user_id exists
 
-# Same as above. This probably won't be called from the outside and so doesn't really need an endpoint -Kieran
-@app.route("/users", methods=["POST"])
-def check_user_exists():
-    # Fetch user_id to check from response
-    data = request.get_json()
-    user_id = data["user_id"]
 
-    # Mock list of user ids
-    user_ids = [37, 43, 1009]
-
-    if user_id in user_ids:
-        return jsonify({"user_exists": True})
-    else:
-        return jsonify({"user_exists": False})
-
-
-@app.route("/answers/username", methods=["GET", "POST"])
+@app.route("/answers/<username>", methods=["GET", "POST"])
 # I suspect this will need to be altered to take a username parameter so we know whose answers we're checking - Kieran
 def get_answers(username):
     if request.method == "GET":
@@ -83,27 +58,13 @@ def get_answers(username):
             return jsonify({"answer_exists": False})
 
 
-# Check if user data is complete
-
-# Another that might not need an endpoint -Kieran
-@app.route("/user_is_complete/<username>", methods=["GET"])
-def user_is_complete(username):
-    # Mock user
-    user = {"Joe": {"questions": []}}
-    if username in user:
-        completed = bool(user[username]["questions"])
-        return jsonify({"is_completed": completed})
-    else:
-        return jsonify({"error": "User not found"})
-
-
 # Method to return all questions to an endpoint /api/questions
 @app.route("/questions", methods=["GET"])
 def get_questions():
     question_ids = []
     return jsonify({"question_ids": question_ids})
 
-
+#Method to check if a user exists and create them if they don't
 @app.route("/user/<username>", methods=["PUT"])
 def user_creation(username):
     if request.method == "PUT":
@@ -119,18 +80,6 @@ def user_creation(username):
         else:
             #user already exists
             return jsonify({"message" : "User exists"}), 204
-        
-        
-
-# DB method implementation      in-progress
-@app.route("/test", methods=["POST"])
-def test_user_create():
-    user_details_object = request.get_json()
-    insert_user(user_details_object["email"], user_details_object["password"])
-
-    return jsonify(user_details_object), 201
-
-
 
 
 #Login endpint:
