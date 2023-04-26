59fc9ed1925c7da3b0776ffdd8a19da4e5a5c939
Evan Krueger <evank0301@gmail.com>
2023-04-26 17:14:34 -0500

    Added submission functionality, only API integration remains

--- roomee-fe/src/components/questionGrid.js
+++ roomee-fe/src/components/questionGrid.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import QuestionTile from './questionTile'
 
-export default function QuestionGrid({Questions}){
+export default function QuestionGrid({Questions, answers, setAnswers}){
+    
+    //Define an array to hold the question Tiles
     var questionArray = []
+
+        //Iterate through the questions that we have been supplied with and create a tile for each
+        //Once a tile has been created, append it to the questionArray
         let questionIdx = 0
         while(questionIdx < Questions.length){
-            var questionInput = <QuestionTile question = {Questions[questionIdx]}></QuestionTile>             
+            var questionInput = <QuestionTile  key = {questionIdx} questionId = {questionIdx} question = {Questions[questionIdx]} answers = {answers} setAnswers = {setAnswers}></QuestionTile>
             questionArray.push(questionInput)
             questionIdx ++
         }
-    
+
+    //Return the quiz in HTML form
     return(
         <div className='quiz'>
             {questionArray}
