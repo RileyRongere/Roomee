f0363314fc393db9181117bc2522f007cb65773d
Amelia Sims <amelia.sims@drake.edu>
2023-04-26 17:19:35 -0500

    comments

--- roomee-fe/src/Pages/password_page.jsx
+++ roomee-fe/src/Pages/password_page.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 
+// need to get username form the other page - how pass? 
 
+function searchPassword() {
+    fetch('api_name')
+    if
+
+    else
+
+    // Conditional statment that sends password and either calls next page or throws an error message 
+}
+
+// we need to change heading to a conditional bassed on if the username is found or not 
 const Password = () => {
     return (
         <div className="Password">
