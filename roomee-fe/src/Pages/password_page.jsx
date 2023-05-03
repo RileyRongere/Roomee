3ce6ada0a9645b2d1014d22f2819101e567f9a98
Evan Krueger <evank0301@gmail.com>
2023-05-02 19:51:29 -0500

    Added a way to pass username

--- roomee-fe/src/Pages/password_page.jsx
+++ roomee-fe/src/Pages/password_page.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
 // need to get username form the other page - how pass?
 
 function searchPassword() {
@@ -13,29 +13,39 @@ function searchPassword() {
 
 // we need to change heading to a conditional bassed on if the username is found or not
 
-function componentDidMount() {
-  console.log(this.props.location.state.username);
-}
+// function componentDidMount() {
+//   console.log(this.props.location.state.username);
+// }
+    // <div className="Password">
+    //   <head>
+    //     <h2> Enter Password Here </h2>
+    //     <form id="password" onSubmit="searchPassword()">
+    //       <input
+    //         type="text"
+    //         id="password"
+    //         name="password"
+    //         placeholder="password"
+    //         size="15"
+    //       >
+    //       </input>
+    //       <button class="button">Submit</button>
+    //     </form>
+    //   </head>
+    // </div>
 
-const Password = () => {
+
+const Password = (props) => {
+  const {state} = useLocation();
+
+  console.log(state.name)
   return (
-    <div className="Password">
-      <head>
-        <h2> Enter Password Here </h2>
-        <form id="password" onSubmit="searchPassword()">
-          <input
-            type="text"
-            id="password"
-            name="password"
-            placeholder="password"
-            size="15"
-          >
-            {" "}
-          </input>
-          <button class="button">Submit</button>
-        </form>
-      </head>
+    <div className = "Password">
+      <h1>Password Page</h1>
+      <form id = "password">
+        <input type = "text" id = "password" name = "password" placeholder = "password"></input>
+      </form>
     </div>
+
   );
 };
 export default Password;
