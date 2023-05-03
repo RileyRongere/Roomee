9d71bd75e19ead64118a9f9dcef8550df9ace18b
Kali <you@example.comkali.zerwas@drake.edu>
2023-05-03 18:04:39 -0500

    Made a few changes to remove function.

--- server/roomee_api/src/matching_algoirthm.py
+++ server/roomee_api/src/matching_algoirthm.py
@@ -8,7 +8,7 @@ def distance(user1, user2, question, score):
 
 
 def remove(user1, user2, q1, q2,user_list):
-    if user1[q1] != user2[q2]:
+    if user1[q1] == 1 and user2[q2]==0:
         user_list.remove(user2)
 
 
@@ -33,13 +33,19 @@ def main():
         score=0
         gender(user1, user, user_list)
         if user in user_list:
-            remove(user1, user2, q1, q2,user_list) #need to change q1 and q2, just put all four in manually?
+            remove(user1, user2, have_pet, want_pet,user_list) 
+        if user in user_list: 
+            remove(user2, user1, have_pet, want_pet,user_list)
+        if user in user_list:
+            remove(user1, user2, smoke, smoke_ok,user_list) 
+        if user in user_list:
+            remove(user2, user1, smoke, smoke_ok,user_list)
         if user in user_list:
             for question in question_list:
                 score = distance(user1, user, question, score) 
-        if score < 3:
+        if score < 3: #Need to be changed
             high.append(user)
-        elif score > 15:
+        elif score > 15: #Need to be changed
             low.append(user)
         else:
             med.append(user)
