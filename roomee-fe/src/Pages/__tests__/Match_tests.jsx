f7c554159d9588cabcc8feef53bfcdfaebbf61d2
Amelia Sims <amelia.sims@drake.edu>
2023-05-03 22:16:21 -0500

    created matching test

--- roomee-fe/src/Pages/__tests__/Match_tests.jsx
+++ roomee-fe/src/Pages/__tests__/Match_tests.jsx
@@ -1,3 +1,79 @@
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
-import Quiz from "../Match";
+import Match from "../Match";
+import previousMatch from "../Match";
+import nextMatch from "../Match";
 import { MemoryRouter } from "react-router-dom";
+
+afterEach(() => {
+    cleanup();
+});
+
+// tests page rendering 
+test("Test that the page renders correctly", () => {
+    render(
+        <MemoryRouter>
+            <Match />
+        </MemoryRouter>
+    );
+    const pageHeader = screen.getByText(/Match Page/i);
+    expect(pageHeader).toBeInTheDocument();
+});
+
+// tests matching rendering 
+test("Test that the matching renders correctly", () => {
+    render(
+        <MemoryRouter>
+            <Match />
+        </MemoryRouter>
+    );
+    const matchDisplay = screen.getByTestId("match-display");
+    expect(matchDisplay).toBeInTheDocument();
+});
+
+// tests previous button 
+test("Test that the previous button works", () => {
+    render(
+        <MemoryRouter>
+            <Match />
+        </MemoryRouter>
+    );
+    const previousButton = screen.getByText(/Previous/i);
+    expect(previousButton).toBeInTheDocument();
+
+    console.log = jest.fn();
+
+    fireEvent.click(previousButton);
+    expect(console.log.mock.calls[0][0]).toBe(previousMatch);
+});
+
+// tests next button 
+test("Test that the next button works", () => {
+    render(
+        <MemoryRouter>
+            <Match />
+        </MemoryRouter>
+    );
+    const nextButton = screen.getByText(/Next/i);
+    expect(nextButton).toBeInTheDocument();
+
+    console.log = jest.fn();
+
+    fireEvent.click(nextButton);
+    expect(console.log.mock.calls[0][0]).toBe(nextMatch);
+});
+
+// tests edit button 
+test("Test that the edit button works", () => {
+    render(
+        <MemoryRouter>
+            <Match />
+        </MemoryRouter>
+    );
+    const editButton = screen.getByText(/Edit Profile/i);
+    expect(editButton).toBeInTheDocument();
+
+    console.log = jest.fn();
+
+    fireEvent.click(editButton);
+    expect(console.log.mock.calls[0][0]).toBe("/quiz");
+});
