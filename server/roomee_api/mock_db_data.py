43d972589f0def679e9fdaaec68a5ba2a67a6709
Theodore Bartemes <taybartemes@gmail.com>
2023-05-05 21:29:09 -0500

    adding missing files

---
+++ server/roomee_api/mock_db_data.py
@@ -0,0 +1,112 @@
+# Authored by priscilla Tuazon
+from backend_py_objects import *
+from insert_query_DB import *
+import mysql.connector
+
+db = mySQL()
+
+
+def main():
+    # (Email, Password)
+    # insert some users
+    db.insert_user("alice@email.com", "alice")  # ID = 9
+    db.insert_user("bob@email.com", "bob")  # ID = 10
+    db.insert_user("maddie@email.com", "maddie")  # ID = 11
+
+    # (question)
+    # insert some questions
+    #db.insert_question("How often do you smoke?")
+    #db.insert_question("Are you a procrastinator?")
+    #db.insert_question("How neat are you?")
+    db.insert_question("When do you go to sleep?")
+    db.insert_question("When do you wake up?")
+    db.insert_question("Do you have any pets?")
+    db.insert_question("Are you open to living with pets?")
+    db.insert_question("How often do you like to invite friends over?")
+    db.insert_question("Do you smoke?")
+    db.insert_question("Are you comfortable with your roommate smoking?")
+    db.insert_question("Are you introverted or extroverted")
+    db.insert_question("How clean do you keep your space?")
+    db.insert_question("How loud do you like your space?")
+    db.insert_question("How often are you in your space?")
+    db.insert_question("How busy are you during the week?")
+    db.insert_question("How busy are you during the weekend?")
+    db.insert_question("What is your age?")
+    db.insert_question("What is your gender?")
+    db.insert_question("What is your preferred gender to live with?")
+# (Question_ID, User_ID, Answer)
+    # insert some answers
+    #Sleep
+    db.insert_answer(9, 9, 3)
+    db.insert_answer(9, 10, 1)
+    db.insert_answer(9, 11, 4)
+#Wake up
+    db.insert_answer(10, 9, 5)
+    db.insert_answer(10, 10, 4)
+    db.insert_answer(10, 11, 4)
+#Pets
+    db.insert_answer(11, 9, 1)
+    db.insert_answer(11, 10, 0)
+    db.insert_answer(11, 11, 0)
+#ok with pets
+    db.insert_answer(12, 9, 1)
+    db.insert_answer(12, 10, 1)
+    db.insert_answer(12, 11, 0)
+#friends
+    db.insert_answer(13, 9, 4)
+    db.insert_answer(13, 10, 3)
+    db.insert_answer(13, 11, 2)
+#smoking
+    db.insert_answer(14, 9, 0)
+    db.insert_answer(14, 10, 0)
+    db.insert_answer(14, 11, 0)
+#ok with smoking
+    db.insert_answer(15, 9, 0)
+    db.insert_answer(15, 10, 0)
+    db.insert_answer(15, 11, 0)
+#into or extro
+    db.insert_answer(16, 9, 1)
+    db.insert_answer(16, 10, 0)
+    db.insert_answer(16, 11, 1)
+#clean
+    db.insert_answer(17, 9, 5)
+    db.insert_answer(17, 10, 2)
+    db.insert_answer(17, 11, 5)
+#noise
+    db.insert_answer(18, 9, 0)
+    db.insert_answer(18, 10, 2)
+    db.insert_answer(18, 11, 4)
+#how often in space
+    db.insert_answer(19, 9, 1)
+    db.insert_answer(19, 10, 1)
+    db.insert_answer(19, 11, 4)
+#busy during week
+    db.insert_answer(20, 9, 2)
+    db.insert_answer(20, 10, 2)
+    db.insert_answer(20, 11, 4)
+#busy during weekend
+    db.insert_answer(21, 9, 1)
+    db.insert_answer(21, 10, 3)
+    db.insert_answer(21, 11, 4)
+#age
+    db.insert_answer(22, 9, 2)
+    db.insert_answer(22, 10, 2)
+    db.insert_answer(22, 11, 3)
+#gender
+    db.insert_answer(23, 9, 0)
+    db.insert_answer(23, 10, 0)
+    db.insert_answer(23, 11, 0)
+#pref gender
+    db.insert_answer(24, 9, 3)
+    db.insert_answer(24, 10, 3)
+    db.insert_answer(24, 11, 3)
+
+    # (User1, User2, Percent_Match)
+    # insert some matches
+    db.insert_match(9, 10, 75)
+    db.insert_match(9, 11, 40)
+    db.insert_match(10, 11, 10)
+
+
+if __name__ == "__main__":
+    main()
--- server/roomee_api/src/matching_algorithm.py
+++ server/roomee_api/src/matching_algorithm.py
@@ -1,60 +1,129 @@
 #Matching Algorithm
+import mysql.connector
+#Load Data:
+cnx = mysql.connector.connect(user='MYSQL_USER', password='MYSQL_PASSWORD',
+                              host='localhost',
+                              database='roomee',
+                              port='9906')
+
+# queries for data
+
+# user_data = cnx.query_user_by_email('alice@email.com')
+# user2_data = cnx.query_user_by_email('bob@email.com')
+
+# print(user_data)
+
+
+query = "SELECT * FROM USER"
+
+
+# Execute query
+with cnx.cursor() as cursor:
+    cursor.execute(query)
+
+    # Print each row of results
+    user_data2=[]
+    for row in cursor:
+        user_data2.append(row)
+        user1=row #IDK how to actually get the user that had signed in yet
+
+query2= "SELECT * FROM ANSWER"
+
+with cnx.cursor() as cursor:
+    cursor.execute(query2)
+
+    # Print each row of results
+    answer_data=[]
+    for row in cursor:
+        answer_data.append(row)
+
+
+
+# closing database
+
+
+
 
 #New plan
 
-def distance(user1, user2, question, score):
-    score=abs(user1[question]-user2[question])
+def distance(user1, user2, question, answer_list):
+    for answer in answer_list:
+        if answer[2]==user1 and answer[1]==question:
+            user1_answer=answer[3]
+        if answer[2]==user2 and answer[1]==question:
+            user2_answer=answer[3]
+    score=abs(user1_answer-user2_answer)
     return score
 
 
-def remove(user1, user2, q1, q2,user_list):
-    if user1[q1] == 1 and user2[q2]==0:
-        user_list.remove(user2)
+def remove(user1, user2, q1, q2, user_list, user, answer_list):
+    for answer in answer_list:
+        if answer[2]==user1 and answer[1]==q1:
+            user1_1=answer[3]
+        if answer[2]==user2 and answer[1]==q2:
+            user2_2=answer[3]
+    if user1_1 == 1 and user2_2==0:
+        user_list.remove(user)
 
 
 
-def gender(user1, user2, user_list):
-    if user1["pref_gener"] != user2["gender"] and user2["pref_gender"] != user1["gender"]:
-        user_list.remove(user2)
-    if user1["gender"] == 3 and user2["pref_gender"] == 3:
-        user_list.append(user2)
+def gender(user1, user2, user_list, user, answer_list):
+    #print(answer_list)
+    for answer in answer_list:
+        #print(answer)
+        if answer[2]==user1 and answer[1]==23: #question has to be chagned to whatever the gender question id would be 
+            user1_gender_answer=answer[3]
+        if answer[2]==user2 and answer[1]==23: #question has to be chagned to whatever the gender question id would be 
+            user2_gender_answer=answer[3]
+        if answer[2]==user1 and answer[1]==24: #question has to be chagned to whatever the gender preference question id would be 
+            user1_pref_answer=answer[3]
+        if answer[2]==user2 and answer[1]==24: #question has to be chagned to whatever the gender preference question id would be 
+            user2_pref_answer=answer[3]
+    if user1_gender_answer != user2_pref_answer and user2_gender_answer != user1_pref_answer:
+        user_list.remove(user)
+    if user1_pref_answer == 3 and user2_pref_answer == 3:
+        user_list.append(user)
 
-#mock user list
-user_list=[Jilly, John, Joel]
-user1=Kara
 
-question_list=[id_1,id_2,id_3] #Needs to be a list that does not include pet, gender, and amoking questions
 
-def main():
+
+
+
+def match(user1, user_list, answer_list2):
     high=[]
     med=[]
     low=[]
+    answer_list3=answer_list2.copy()
+    answer_list = [answer for answer in answer_list2 if answer[1] not in [11, 12, 14, 15, 24]]
+    final_score=0
     for user in user_list:
-        score=0
-        gender(user1, user, user_list)
         if user in user_list:
-            remove(user1, user2, have_pet, want_pet,user_list) 
+            gender(user1[0], user[0], user_list, user, answer_list3)
+        if user in user_list:
+            remove(user1[0], user[0], 11, 12,user_list,user, answer_list3) #Change to be actual id numbers
         if user in user_list: 
-            remove(user2, user1, have_pet, want_pet,user_list)
+            remove(user[0], user1[0], 11, 12,user_list,user, answer_list3) #Change to be actual id numbers
         if user in user_list:
-            remove(user1, user2, smoke, smoke_ok,user_list) 
+            remove(user1[0], user[0], 14, 15,user_list,user, answer_list3) #Change to be actual id numbers
         if user in user_list:
-            remove(user2, user1, smoke, smoke_ok,user_list)
-        if user in user_list:
-            score1=intro_extro(user1,user,score)
-            score2=friends_over(user1,user,score1)
-            score3=wake_up(user1,user,score2)
-            final_score=sleep(user1,user,score3)
-            if final_score < 5:
-                low.append(user)
-            elif final_score > 0:
-                high.append(user)
-            else:
-                med.append(user)
-
-
-
-
-
-
+            remove(user[0], user1[0], 14, 15,user_list,user, answer_list3) #Change to be actual id numbers
         
+    for user in user_list:
+
+        for answer in answer_list:
+            if user[0] == answer[2]:
+                    if user in user_list:
+                        score=distance(user1[0], user[0], answer[1], answer_list)
+                        final_score=final_score+score
+        if final_score > 30:
+            low.append(user)
+        elif final_score  < 15:
+            high.append(user)
+        else:
+            med.append(user)
+    print("high", high, "med", med, "low", low)
+
+
+match(user1, user_data2, answer_data)
+
+cnx.close()
