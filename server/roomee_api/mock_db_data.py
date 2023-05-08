70e81baa02721a2ba182df0dcee0631b09ccd93d
Kali <you@example.comkali.zerwas@drake.edu>
2023-05-08 12:48:10 -0500

    ran black

--- server/roomee_api/mock_db_data.py
+++ server/roomee_api/mock_db_data.py
@@ -15,9 +15,9 @@ def main():
 
     # (question)
     # insert some questions
-    #db.insert_question("How often do you smoke?")
-    #db.insert_question("Are you a procrastinator?")
-    #db.insert_question("How neat are you?")
+    # db.insert_question("How often do you smoke?")
+    # db.insert_question("Are you a procrastinator?")
+    # db.insert_question("How neat are you?")
     db.insert_question("When do you go to sleep?")
     db.insert_question("When do you wake up?")
     db.insert_question("Do you have any pets?")
@@ -34,69 +34,69 @@ def main():
     db.insert_question("What is your age?")
     db.insert_question("What is your gender?")
     db.insert_question("What is your preferred gender to live with?")
-# (Question_ID, User_ID, Answer)
+    # (Question_ID, User_ID, Answer)
     # insert some answers
-    #Sleep
+    # Sleep
     db.insert_answer(9, 9, 3)
     db.insert_answer(9, 10, 1)
     db.insert_answer(9, 11, 4)
-#Wake up
+    # Wake up
     db.insert_answer(10, 9, 5)
     db.insert_answer(10, 10, 4)
     db.insert_answer(10, 11, 4)
-#Pets
+    # Pets
     db.insert_answer(11, 9, 1)
     db.insert_answer(11, 10, 0)
     db.insert_answer(11, 11, 0)
-#ok with pets
+    # ok with pets
     db.insert_answer(12, 9, 1)
     db.insert_answer(12, 10, 1)
     db.insert_answer(12, 11, 0)
-#friends
+    # friends
     db.insert_answer(13, 9, 4)
     db.insert_answer(13, 10, 3)
     db.insert_answer(13, 11, 2)
-#smoking
+    # smoking
     db.insert_answer(14, 9, 0)
     db.insert_answer(14, 10, 0)
     db.insert_answer(14, 11, 0)
-#ok with smoking
+    # ok with smoking
     db.insert_answer(15, 9, 0)
     db.insert_answer(15, 10, 0)
     db.insert_answer(15, 11, 0)
-#into or extro
+    # into or extro
     db.insert_answer(16, 9, 1)
     db.insert_answer(16, 10, 0)
     db.insert_answer(16, 11, 1)
-#clean
+    # clean
     db.insert_answer(17, 9, 5)
     db.insert_answer(17, 10, 2)
     db.insert_answer(17, 11, 5)
-#noise
+    # noise
     db.insert_answer(18, 9, 0)
     db.insert_answer(18, 10, 2)
     db.insert_answer(18, 11, 4)
-#how often in space
+    # how often in space
     db.insert_answer(19, 9, 1)
     db.insert_answer(19, 10, 1)
     db.insert_answer(19, 11, 4)
-#busy during week
+    # busy during week
     db.insert_answer(20, 9, 2)
     db.insert_answer(20, 10, 2)
     db.insert_answer(20, 11, 4)
-#busy during weekend
+    # busy during weekend
     db.insert_answer(21, 9, 1)
     db.insert_answer(21, 10, 3)
     db.insert_answer(21, 11, 4)
-#age
+    # age
     db.insert_answer(22, 9, 2)
     db.insert_answer(22, 10, 2)
     db.insert_answer(22, 11, 3)
-#gender
+    # gender
     db.insert_answer(23, 9, 0)
     db.insert_answer(23, 10, 0)
     db.insert_answer(23, 11, 0)
-#pref gender
+    # pref gender
     db.insert_answer(24, 9, 3)
     db.insert_answer(24, 10, 3)
     db.insert_answer(24, 11, 3)
