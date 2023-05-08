a4fc68a802ab93ded9b41e42a580eb9a0c34edc3
Jack Messerli-Wallace <jack.messerli-wallace@drake.edu>
2023-05-08 11:39:37 -0500

    Put api_team api calls into roomee_api.py

--- server/roomee_api/src/roomee_api.py
+++ server/roomee_api/src/roomee_api.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import os
 import json
 import sys
+from src.insert_query_DB import mySQL
 
 app = Flask(__name__)
 
@@ -115,52 +116,52 @@ def is_valid(dirty_string):
 
 
 @app.route("/answers/<username>", methods=["GET", "POST"])
-# I suspect this will need to be altered to take a username parameter so we know whose answers we're checking - Kieran
-def get_answers(username):
+def get_answers():
+    db_conn = mySQL()
     if request.method == "GET":
-        # Mock list of user answers
-        # answer_ids = []
+        # Fetch stuff from call
+        data = request.get_json()
+        user_id = data.get("user_id")
 
-        # return jsonify({"answer_ids": answer_ids})
-        print("GET ANSWERS CALL")
-        print(username)
-        answers = mocked_get_answers(username)
-        return jsonify({"answers": answers})
+        answers = db_conn.query_answer(user_id)
+
+        return jsonify(answers)
 
     if request.method == "POST":
-        # Fetch answer_id to check
+        # Fetch stuff from call
         data = request.get_json()
-        answer_id = data["answer_id"]
+        user_id = data.get("user_id")
+        question_ids = data.get("question_id")  # list
+        answers = data.get("answers")  # list
 
-        # Mock list of answer ids
-        answer_ids = [27, 8, 90]
+        for i in question_ids:
+            db_conn.insert_answer(i, user_id, answers[i])
 
-        if answer_id in answer_ids:
-            return jsonify({"answer_exists": True})
-        else:
-            return jsonify({"answer_exists": False})
+        return jsonify({"message": "Answers successfully posted"}), 201
 
 
 # Method to return all questions to an endpoint /api/questions
 @app.route("/questions", methods=["GET"])
 def get_questions():
-    # question_ids = []
-    # return jsonify({"question_ids": question_ids})
-
-    questions = mocked_get_questions()
-    return jsonify({"questions": questions})
+    db_conn = mySQL()
+    if request.method == "GET":
+        questions = db_conn.query_all_questions()
+        return jsonify({"questions": questions}), 200
 
 
 # Method to check if a user exists and create them if they don't
-@app.route("/user/<username>", methods=["PUT"])
-def user_creation(username):
+@app.route("/user/<email>", methods=["PUT"])
+def user_creation(email):
+    db_conn = mySQL()
     if request.method == "PUT":
-        user = mocked_user(username)
+        user = get_user(email)
 
         if user == {}:
             # user doesn't exist
+            # create entry for user
+            db_conn.insert_user(email, "")
 
-            return jsonify({"message": "User does not exist"}), 200
+            return jsonify({"message": "User created."}), 200
 
         else:
             # user already exists
@@ -175,38 +176,94 @@ def user_creation(username):
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    username = data.get("username")
+    email = data.get("email")
     password = data.get("password")
 
-    print(data, file=sys.stderr)
-
     # Is this what you mean by sanitize? I am removing any leading/trailing whitespace
-    if is_valid(username) and is_valid(password):
-        user = mocked_user(username)
-        print(user, file=sys.stderr)
+    if is_valid(email) and is_valid(password):
+        user = get_user(email)
 
-        if not user == {} and user[1]["password"] == password:
-            return jsonify({"message": "Login successful."}), 200
+        if not user == {} and user["password"] == password:
+            return jsonify({"user": user, "message": "Login successful."}), 200
         else:
-            return jsonify({"message": "Invalid username or password."}), 403
+            return jsonify({"message": "Invalid email or password."}), 403
 
     else:
-        return jsonify({"message": "Invalid username or password format"}), 400
+        return jsonify({"message": "Invalid email or password format"}), 400
 
 
 # register endpoint to take in and save username and password
 @app.route("/register", methods=["POST"])
 def register_user():
+    db_conn = mySQL()
     data = request.get_json()
-
-    username = data.get("username")
+    email = data.get("email")
     password = data.get("password")
 
     # sanitize data
-    if is_valid(username) and is_valid(password):
-        mocked_user_insert(username, password)
+    if is_valid(email) and is_valid(password):
+        db_conn.insert_user(email, password)
 
-        return jsonify({"message": "User created"}), 200
+        user = get_user(email)
+
+        return jsonify({"user": user, "message": "User created"}), 200
 
     else:
-        return jsonify({"message": "Invalid username or password format"}), 400
+        return jsonify({"message": "Invalid email or password format"}), 400
+
+
+# Function to generate match in format frontend wants
+def match_format(id):
+    db_conn = mySQL()
+    qid = []  # temp question id
+    question = []  # temp question
+    answers = []  # temp answers
+
+    curr_answers = db_conn.query_answer(id)  # list
+    # [{AnswerID, UserID, QuestionID, Answer}]
+
+    qid = [q["QuestionID"] for q in curr_answers]  # question IDs
+    for j in qid:
+        question.append(
+            db_conn.query_question(j["QuestionID"])["Question"]
+        )  # add question
+        # this seems faster than iterating through the all_questions variable that
+        # is commented out.
+
+    answers = [q["Answer"] for q in curr_answers]  # answers
+
+    return {
+        "question_ids": qid,
+        "questions": question,
+        "answers": answers,
+    }  # format frontend wants
+
+
+# match endpoint
+@app.route("/matches", methods=["GET"])
+def get_matches():
+    # should output in way depicted by frontend
+    db_conn = mySQL()
+    data = request.get_json()
+    user_id = data.get("user_id")
+
+    input_info = match_format(user_id)  # inputted person's question data
+
+    matches = db_conn.query_matches(user_id)  # list of dicts
+    # [{MatchID, UserID_1, UserID_2, PercentMatch}]
+
+    # all_questions = db_conn.query_all_questions() #all questions for reference
+    # [{QuestionID, Question}]
+
+    # need to query answers for every user
+    match_dict = {}
+    for i in matches:
+        curr_id = i["UserID_2"]  # current match id
+        curr_match = match_format(curr_id)
+        curr_match["matchScore"] = i["PercentMatch"]
+        match_dict[curr_id] = curr_match
+
+    return (
+        jsonify({user_id: input_info, "matches": match_dict}),
+        200,
+    )  # as frontend desires
